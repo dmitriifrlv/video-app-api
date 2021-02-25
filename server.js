90e39f000bde8c822bdab983389a9a1111bfe5fc
Dmitrii Frolov <dmitriifrolov@Dmitriis-MacBook-Pro.local>
2021-02-25 16:26:41 +0500

    initial commit

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors')
+app.use(bodyParser.json());
+app.use(cors())
+const database = {
+  users: [
+    {
+      id: 1,
+      login: 'admin',
+      password: 'qwerty'
+    }
+  ]
+}
+// app.use(express.json())
+
+// app.use(express.static('public'))
+const jwtsecret = "secret"
+// app.post('/login', (req, res))
+app.post('/signin', (req, res) => {
+  if (req.body.login === database.users[0].login && req.body.password === database.users[0].password) {
+    res.json(
+      {
+      status:"success",
+      token: jwt.sign({ name: "admin", color: "green"}, jwtsecret)
+      })
+  } else {
+    res.status(400).json('error logging in')
+  }
+})
+
+app.listen(3000, ()=>{
+  console.log('so far so good')
+})
